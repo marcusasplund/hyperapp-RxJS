3fb5c6dac0b9df8fa5f9f9cf95ba9184f79a6445
Marcus Asplund <rootfood@gmail.com>
2017-03-26 01:06:34 +0100

    :star: :sheep: update desc

--- app/view.js
+++ app/view.js
@@ -10,5 +10,6 @@ export const view = (model, actions) =>
       <h3>{model.count}</h3>
       {addButton(actions)}
       {subButton(actions)}
+      {<p>click away, clicks will be captured and actions will be queued up, then executed with minimum 600ms between each</p>}
       {showConsole()}
   </div>
