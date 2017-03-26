855f7ddd308e037b423a2deb7a3ccc68033ad225
System <none>
2017-03-26 00:10:04 +0000

    😇👽 Updated with Glitch

--- app/view.js
+++ app/view.js
@@ -5,10 +5,11 @@ import {showConsole} from '../views/show-console'
 
 export const view = (model, actions) =>
   <div class='container'>
-    <h2>hyperapp rxjs <a href=''><small>source</small></a></h2>
+    <h2>hyperapp rxjs <a href='https://github.com/marcusasplund/hyperapp-RxJS'><small>source</small></a></h2>
      <hr/>
       <h3>{model.count}</h3>
       {addButton(actions)}
       {subButton(actions)}
+      {<p>click away, clicks will be captured and actions will be queued up, then executed with minimum 600ms between each</p>}
       {showConsole()}
   </div>
