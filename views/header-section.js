2873c383a5d93f712ce497dbba5c37a92f404de7
Marcus Asplund <rootfood@gmail.com>
2017-03-29 09:26:11 +0200

    :sheep: :dog: fixed fixes

--- views/header-section.js
+++ views/header-section.js
@@ -1,8 +1,17 @@
 import {h} from 'hyperapp'
 
-export const headerSection = (model) =>
+export const headerSection = () =>
   <header>
     <h2>hyperapp RxJS </h2>
-    <p><small><em>click away, clicks will be captured and actions will be queued up, then executed with minimum 600ms between each <a href='https://github.com/marcusasplund/hyperapp-RxJS'></em>source</small></a></p>
+    <p>
+      <small>
+        <em>
+          click away, clicks will be captured and actions will be queued up, then executed with minimum 600ms between each
+        </em>&nbsp; 
+          <a href='https://github.com/marcusasplund/hyperapp-RxJS'>
+             source
+          </a>
+      </small>
+    </p>
     <hr/>
   </header>
