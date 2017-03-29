e11b1f10d8fed020b4fdb337a65c11c6d23fc0dd
Marcus Asplund <rootfood@gmail.com>
2017-03-29 09:17:13 +0200

    :beer: :cow: separate header section

---
+++ views/header-section.js
@@ -0,0 +1,8 @@
+import {h} from 'hyperapp'
+
+export const headerSection = (model) =>
+  <header>
+    <h2>hyperapp RxJS </h2>
+    <p><small><em>click away, clicks will be captured and actions will be queued up, then executed with minimum 600ms between each <a href='https://github.com/marcusasplund/hyperapp-RxJS'></em>source</small></a></p>
+    <hr/>
+  </header>
