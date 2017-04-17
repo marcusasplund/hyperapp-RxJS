e946ea8e8335320a41f5eab7419da5303b2c507e
Marcus Asplund <rootfood@gmail.com>
2017-04-17 21:39:00 +0200

    Create index.js

---
+++ src/views/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import {h} from 'hyperapp'
+import {HeaderSection} from '../views/header-section'
+import {AddButton} from '../views/add-button'
+import {SubButton} from '../views/sub-button'
+import {ShowConsole} from '../views/show-console'
+
+export const view = (store, actions) =>
+  <div class='container'>
+    <HeaderSection />
+    <AddButton actions={actions} />
+    <SubButton actions={actions} />
+    <ShowConsole />
+  </div>
+  
