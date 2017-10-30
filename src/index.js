7fddf90d7f9ff37cac4867dfb641f0bee6daaaab
Marcus Asplund <rootfood@gmail.com>
2017-10-30 12:54:03 +0100

    rolled back serviceworker

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import {app} from 'hyperapp'
 import {actions} from './actions/'
 import {state} from './state/'
 import {view} from './views/'
+import {registerWorker} from './utils/register-worker'
 
 app({
   actions,
   state,
   view
 })
+// Register service worker if not on localhost
+const local = window.location.host.startsWith('localhost')
+
+if ('serviceWorker' in navigator && !local) {
+  registerWorker()
+}
