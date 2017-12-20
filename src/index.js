6d4611bdcd4e0467e3f1ef427fc73f2e1a1a760d
Marcus Asplund <rootfood@gmail.com>
2017-12-20 15:26:46 +0100

    :star: :fire: update Ha to 0.18.0

--- src/index.js
+++ src/index.js
@@ -3,26 +3,24 @@ import {view} from './views/'
 import {delayedClicks$} from './utils/delayed-click'
 import {registerWorker} from './utils/register-worker'
 
-const model = {
-  state: {
-    count: 0
+const state = {
+  count: 0
+}
+const actions = {
+  add: () => state => ({
+    count: ++state.count
+  }),
+  sub: () => state => ({
+    count: --state.count
+  }),
+  scheduleAdd: () => (state, actions) => {
+    delayedClicks$.next(actions.add)
   },
-  actions: {
-    add: () => state => ({
-      count: ++state.count
-    }),
-    sub: () => state => ({
-      count: --state.count
-    }),
-    scheduleAdd: () => state => {
-      delayedClicks$.next(actions.add)
-    },
-    scheduleSub: () => state => {
-      delayedClicks$.next(actions.sub)
-    }
+  scheduleSub: () => (state, actions) => {
+    delayedClicks$.next(actions.sub)
   }
 }
 
-const { actions } = app(model, view, document.body)
+window.main = app(state, actions, view, document.getElementById('app'))
 
 registerWorker()
